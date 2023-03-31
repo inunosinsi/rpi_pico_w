f600180ff6e4a95082dde9129d0ab63cbe5ae951
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2023-03-31 16:59:22 +0900

    Update 2.py

--- wifi/2.py
+++ wifi/2.py
@@ -1,8 +1,11 @@
+# document
+# https://micropython-docs-ja.readthedocs.io/ja/latest/esp8266/tutorial/network_tcp.html#simple-http-server
 
 import network
 import time
 import socket
 
+# ssidは必ず2.4GHz帯の方
 ssid = "your ssid"
 pw = "your pw"
 
@@ -21,8 +24,8 @@ if wlan.status() != 3:
     raise RuntimeError("network connection failed")
 else:
     print("Connected")
-    cnf = wlan.ifconfig()
-    print("ip = " + cnf[0])
+    ifcnf = wlan.ifconfig()
+    print("ip = " + ifcnf[0])
     
 addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
 sock = socket.socket()
@@ -31,15 +34,10 @@ sock.listen(1)
 print('listening on', addr)
 
 while True:
-    try: 
-        conn, addr = sock.accept()
-        print('client connected from', addr)
+    cl, addr = sock.accept()
+    print('client connected from', addr)
         
-        # Create and send response
-        conn.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
-        conn.send("<!DOCTYPE><html><body>hello rpi pico w</body></html>")
-        conn.close()
-        
-    except OSError as e:
-        conn.close()
-        print('connection closed')
+    # Create and send response
+    cl.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
+    cl.send("<!DOCTYPE><html><body>hello rpi pico w</body></html>\n")
+    cl.close()
